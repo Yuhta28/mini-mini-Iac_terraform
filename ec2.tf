e2c2cbf7c57e753be14419dc3933527c87f4d25e
Yuta <yuta_mori@outlook.jp>
2019-10-09 00:38:28 +0900

    destroyEC2

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "SampleTerra" {
-  count       = 3
+  count       = 1
   ami         = "ami-785c491f"  # Ubuntu 16.04 LTS official ami
   instance_type = "t2.micro"
 
   tags = {
-   Name = "${format("SampleTerra-%02d", count.index + 1)}"
+   Name = "${format("modified SampleTerra-%02d", count.index + 1)}"
   }
 }
