742d408da246992b4cd8840e33105d87b147df67
Yuta <yuta_mori@outlook.jp>
2022-02-14 22:21:01 +0900

    renewal

--- README.md
+++
@@ -1,1 +0,0 @@
-# mini-mini-Iac_terraform
--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "SampleTerra" {
-  count       = 1
-  ami         = "ami-785c491f"  # Ubuntu 16.04 LTS official ami
-  instance_type = "t2.micro"
-
-  tags = {
-   Name = "${format("modified SampleTerra-%02d", count.index + 1)}"
-  }
-}
