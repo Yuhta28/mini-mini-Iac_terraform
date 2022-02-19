bc121e1aaf5e554a7609bdb03f16662b1107eeba
Yuta <yuta_mori@outlook.jp>
2022-02-20 01:00:09 +0900

    fix name

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "terraform-vpc" {
 
   cidr_block = "192.168.0.0/16"
   tags = {
-    Name      = "terraform-vpc",
+    Name      = "terraform-vpc-space-lift",
     Terraform = "True"
   }
   enable_dns_hostnames = true
