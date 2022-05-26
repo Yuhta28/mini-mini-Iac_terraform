d6d1ca848016cc4f33cc750a0ebf36a2256d4658
Yuta <yuta_mori@outlook.jp>
2022-05-26 20:32:54 +0900

    create

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "terraform-vpc" {
 
   cidr_block = "192.168.0.0/16"
   tags = {
-    Name      = "terraform-vpc-space-test",
+    Name      = "terraform-vpc-spacelift",
     Terraform = "True"
   }
   enable_dns_hostnames = true
